0642db9abf69871dcc274175f953a8527e440451
Kirill Pustovit <pustovit.kirill@mail.ru>
2022-08-11 09:15:38 +0700

    Добавили код второй задачи

---
+++ Seminar_1/Task_2/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine()) ;
+Console.Write("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine()) ;
+int Max;
+
+if(a > b) {
+    Max = a;
+    Console.WriteLine("Наибольшим является число " + Max);
+}
+if(a < b) {
+    Max = b;
+    Console.WriteLine("Наибольшим является число " + Max);
+}
