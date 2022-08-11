d201345c83d0ed12bee691ddd3920dde34eaeb75
Kirill Pustovit <pustovit.kirill@mail.ru>
2022-08-11 11:49:21 +0700

    Добавили код задачи 6

---
+++ Seminar_1/Task_6/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine()) ;
+if ((number % 2) == 0) {
+    Console.WriteLine("Чётное");
+}
+else {
+    Console.WriteLine("НЕ_чётное");
+}
