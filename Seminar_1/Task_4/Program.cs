c8a77c52bdef955981c313035f0380ed9c51c687
Kirill Pustovit <pustovit.kirill@mail.ru>
2022-08-11 11:33:48 +0700

    Добавили код задачи 4

---
+++ Seminar_1/Task_4/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Write("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine()) ;
+Console.Write("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine()) ;
+Console.Write("Введите третье число: ");
+int c = Convert.ToInt32(Console.ReadLine());
+int Max;
+Max = a;
+if (b > a) {
+  Max = b; }
+if (c > Max) {
+    Max = c; 
+    Console.Clear();
+    }
+
+    
+    Console.WriteLine("Введённые числа: " + a + " " + b + " " + c);
+    Console.WriteLine("Максимальным из трёх введённых чисел является: " + Max);
