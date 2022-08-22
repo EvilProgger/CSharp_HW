ea59dd1b33d642cd0a64af10260ad52652914978
Kirill Pustovit <pustovit.kirill@mail.ru>
2022-08-22 12:54:20 +0700

    добавлено решение 34 задачи

---
+++ Seminar_5/Task_34/Program.cs
@@ -0,0 +1,21 @@
+﻿int[] array = new int[8];
+int N = 0;
+Random rnd = new Random();
+
+Console.WriteLine("Исходный массив: ");
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = rnd.Next(100, 1000);
+    Console.Write("{0} ", array[i]);
+}
+
+foreach (int element in array)
+{
+    
+    if (element % 2 == 0)
+    {
+        N++;
+    }
+}
+Console.WriteLine();
+Console.WriteLine("Количество чётных чисел: " + N);
