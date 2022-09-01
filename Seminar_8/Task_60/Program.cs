d354fc35b416c502c709548d30e9b24963afc6e7
Kirill Pustovit <pustovit.kirill@mail.ru>
2022-09-01 13:00:52 +0700

    добавлено решение 60 задачи

---
+++ Seminar_8/Task_60/Program.cs
@@ -0,0 +1,47 @@
+﻿Console.Clear();
+Console.Write("Введите размерность массива (уровни, строки, столбцы) через пробел: ");
+var s = Console.ReadLine()!.Split(" ");
+Console.WriteLine();
+
+int x = Convert.ToInt32(s[0]);
+int y = Convert.ToInt32(s[1]);
+int z = Convert.ToInt32(s[2]);
+
+double[,,] array3D = new double[x, y, z];
+CreateArray(array3D);
+WriteArray(array3D);
+Console.WriteLine("Построение трёхмерного массива завершено!");
+
+
+
+void CreateArray(double[,,] array3D)
+{
+    for (int i = 0; i < array3D.GetLength(0); i++)
+    {
+        for (int j = 0; j < array3D.GetLength(1); j++)
+        {
+            for (int k = 0; k < array3D.GetLength(2); k++)
+            {
+                array3D[i, j, k] = new Random().Next(-10, 100) + new Random().Next(1, 2);
+            }
+        }
+    }
+}
+
+void WriteArray(double[,,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.WriteLine("Уровень № " + (i + 1));
+
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            for (int k = 0; k < matrix.GetLength(2); k++)
+            {
+                Console.Write($"[{matrix[i, j, k]}]" + " ");
+            }
+            Console.WriteLine();
+        }
+        Console.WriteLine();
+    }
+}
