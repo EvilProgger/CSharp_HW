9f9c323d498a9e5e7703ec64f06c40336b45185f
Kirill Pustovit <pustovit.kirill@mail.ru>
2022-09-09 13:19:57 +0700

    добавлены задачи 9 семинара

--- Semianr_9/Task_64/Program.cs
+++ Semianr_9/Task_64/Program.cs
@@ -4,10 +4,11 @@ int n = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите N: ");
 int M = Convert.ToInt32(Console.ReadLine());
 
-string Queue(int n) {
+string Queue(int n)
+{
     string result = String.Empty;
     if (n == 0) return "";
-    else return $"{n}, {Queue(n-1)}";
+    else return $"{n}, {Queue(n - 1)}";
 }
 Console.WriteLine(Queue(n));
 
--- Semianr_9/Task_68/Program.cs
+++ Semianr_9/Task_68/Program.cs
@@ -1,23 +1,23 @@
 ﻿int InputNumber(string input)
 {
-Console.Write(input);
-int output = Convert.ToInt32(Console.ReadLine());
-return output;
+    Console.Write(input);
+    int output = Convert.ToInt32(Console.ReadLine());
+    return output;
 }
 int Akkerman(int M, int N)
 {
-if (M == 0)
-{
-return N + 1;
-}
-else if (N == 0 && M > 0)
-{
-return Akkerman(M - 1, 1);
-}
-else
-{
-return Akkerman(M - 1, Akkerman(M, N - 1));
-}
+    if (M == 0)
+    {
+        return N + 1;
+    }
+    else if (N == 0 && M > 0)
+    {
+        return Akkerman(M - 1, 1);
+    }
+    else
+    {
+        return Akkerman(M - 1, Akkerman(M, N - 1));
+    }
 }
 
 
